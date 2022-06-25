02a3b7802630e2a24bc44cddb4b95c5cc53917d3
ChuanmingXie <ChuanmingXie@outlook.com>
2022-06-25 19:18:58 +0800

    01_前端原生-原生语言-时尚购物商城项目-第6章JS基础-函数|任务

--- 01_Html_Web/ShopFashion/knowledge/Chapter06/js/6-12functionPredefined.js
+++ 01_Html_Web/ShopFashion/knowledge/Chapter06/js/6-12functionPredefined.js
@@ -19,44 +19,47 @@ confirm()
 prompt()
  */
 
-function predefined(){
-    document.write("parseInt('88.9')的执行结果："+parseInt('88.9')+"<br/>");
-    document.write("parseInt('8T9')的执行结果："+parseInt('8T9')+"<br/>");
-    document.write("parseInt('B89')的执行结果："+parseInt('B89')+"<br/>");
-    document.write("parseInt('0x10')的执行结果："+parseInt('0x10')+"<br/>");
-    document.write("parseInt('12',16)的执行结果："+parseInt('12',16)+"<br/>");
-    var str="3000+499*2";
-    document.write("表达式"+str+"的结果是："+evel(str));
-}
+document.write("parseInt('88.9')的执行结果：" + parseInt('88.9') + "<br/>");
+document.write("parseInt('8T9')的执行结果：" + parseInt('8T9') + "<br/>");
+document.write("parseInt('B89')的执行结果：" + parseInt('B89') + "<br/>");
+document.write("parseInt('0x10')的执行结果：" + parseInt('0x10') + "<br/>");
+document.write("parseInt('12',16)的执行结果：" + parseInt('12', 16) + "<br/>");
+var str = "3000+499*2";
+document.write("表达式" + str + "的结果是：" + eval(str));
 
-do {
-    var money=prompt('请输入你的存款余额');
-    if(isNaN(money))
-    {
-        alert('数据输入不合法,请重新输入！');
-    }
-
-    switch (parsetInt(money/10000)) {
-        case 0:
-            if(money<0){
-                alert('难以解决温饱问题')
-            }
-            else{
-                alert('贫下中农,达到低保生活');
-            }
-        case 1: 
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-            alert('富农，衣食无忧坚守一线');
-            break;    
-        default:
-            alert('容易获得');
+function predefined() {
+    do {
+        // var money = prompt('请输入你的存款余额');
+        var money = document.getElementsByTagName('input')[0].value;
+        // var money = parseFloat(money);
+        if (isNaN(money)) {
+            alert('数据输入不合法,请重新输入！');
             break;
-    }
-} while (true);
+        }
+
+        switch (parseInt(money / 10000)) {
+            case 0:
+                if (money < 0) {
+                    alert('难以解决温饱问题')
+                }
+                else {
+                    alert('贫下中农,达到低保生活');
+                }
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                alert('富农，衣食无忧坚守一线');
+                break;
+            default:
+                alert('容易获得');
+                break;
+        }
+    } while (false);
+}
