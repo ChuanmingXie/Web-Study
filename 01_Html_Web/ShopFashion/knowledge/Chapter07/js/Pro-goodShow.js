a0a7c568510104c6b6ea2cd900085285eed80c5e
ChuanmingXie <ChuanmingXie@outlook.com>
2022-07-05 07:17:40 +0800

    01_前端原生-原生语言-时尚购物商城项目-第7章JS对象-任务2

--- 01_Html_Web/ShopFashion/knowledge/Chapter07/js/Pro-goodShow.js
+++ 01_Html_Web/ShopFashion/knowledge/Chapter07/js/Pro-goodShow.js
@@ -1,60 +1,60 @@
 /* @by chuanmingxie */
-var hotGoods={
-	name:"热门推荐",
-	srcPath:"images/shopshow/",
-	data:[
-		{href:"#",src:"s1.jpg",price:56.00},
-		{href:"#",src:"s2.jpg",price:97.00},
-		{href:"#",src:"s3.jpg",price:56.00},
-		{href:"#",src:"s4.jpg",price:94.00},
-		{href:"#",src:"s5.jpg",price:56.00},
-		{href:"#",src:"s6.jpg",price:56.00},
-		{href:"#",src:"s7.jpg",price:57.00},
-		{href:"#",src:"s8.jpg",price:56.00},
-		{href:"#",src:"s9.jpg",price:32.00},
-		{href:"#",src:"yifu1.jpg",price:56.00},
-		{href:"#",src:"yifu2.jpg",price:56.00},
-		{href:"#",src:"yifu3.jpg",price:76.00},
-		{href:"#",src:"yifu4.jpg",price:36.00},
-		{href:"#",src:"yifu5.jpg",price:196.00}]
+var hotGoods = {
+	name: "热门推荐",
+	srcPath: "images/shopshow/",
+	data: [
+		{ href: "#", src: "s1.jpg", price: 56.00 },
+		{ href: "#", src: "s2.jpg", price: 97.00 },
+		{ href: "#", src: "s3.jpg", price: 56.00 },
+		{ href: "#", src: "s4.jpg", price: 94.00 },
+		{ href: "#", src: "s5.jpg", price: 56.00 },
+		{ href: "#", src: "s6.jpg", price: 56.00 },
+		{ href: "#", src: "s7.jpg", price: 57.00 },
+		{ href: "#", src: "s8.jpg", price: 56.00 },
+		{ href: "#", src: "s9.jpg", price: 32.00 },
+		{ href: "#", src: "yifu1.jpg", price: 56.00 },
+		{ href: "#", src: "yifu2.jpg", price: 56.00 },
+		{ href: "#", src: "yifu3.jpg", price: 76.00 },
+		{ href: "#", src: "yifu4.jpg", price: 36.00 },
+		{ href: "#", src: "yifu5.jpg", price: 196.00 }]
 };
 
-function showHotGoods(){
-	var adviseContent='<h1 class="notice_title">'+hotGoods.name+'</h1><ul class="pic_list3">';
-	var turnShow=getRandomNum(9,0,14);
+function showHotGoods() {
+	var adviseContent = '<h1 class="notice_title">' + hotGoods.name + '</h1><ul class="pic_list3">';
+	var turnShow = getRandomNum(9, 0, 14);
 	for (var i = 0; i < turnShow.length; i++) {
-		var index= turnShow[i];
-		adviseContent+='<li><a href="'+hotGoods.data[index].href+'"><img src="'
-		+hotGoods.srcPath+hotGoods.data[index].src+'" width="80" height="80" ></a><p class="price2">￥'
-		+hotGoods.data[index].price+'元</p></li>';
+		var index = turnShow[i];
+		adviseContent += '<li><a href="' + hotGoods.data[index].href +
+			'"><img src="' + hotGoods.srcPath + hotGoods.data[index].src + '" width="80" height="80" >' +
+			'</a><p class="price2">￥' + hotGoods.data[index].price + '元</p></li>';
 	}
-	adviseContent+='</ul>';
-	document.getElementsByClassName('right_nav')[0].innerHTML=adviseContent;
+	adviseContent += '</ul>';
+	document.getElementsByClassName('right_nav')[0].innerHTML = adviseContent;
 }
 // 返回num个重复的随机数，范围在minNum~maxNum之间
-function getRandomNum(num,minNum,maxNum){
-	var array=new Array();
+function getRandomNum(num, minNum, maxNum) {
+	var array = new Array();
 	for (var i = 0; i < num; i++) {
-		do{
-			var randomNum=Math.floor(Math.random()*maxNum+minNum);
-			if(!checkNum(array,randomNum)){
+		do {
+			var randomNum = Math.floor(Math.random() * maxNum + minNum);
+			if (!checkNum(array, randomNum)) {
 				array.push(randomNum);
 				break;
 			}
-		}while(true);
+		} while (true);
 	}
 	return array;
 }
 /* 数组中包含num个数时返回true，否则返回false */
-function checkNum(array,num){
+function checkNum(array, num) {
 	for (var i = 0; i < array.length; i++) {
-		if(array[i]==num){
+		if (array[i] == num) {
 			return true;
 		}
 	}
 	return false;
 }
 
-window.onload=showHotGoods;
+window.onload = showHotGoods;
 
-window.setInterval("showHotGoods()",2000);
+window.setInterval("showHotGoods()", 2000);
