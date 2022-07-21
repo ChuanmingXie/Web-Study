bcf4798ab091eea0a5dfa4400b2405f169742a8e
ChuanmingXie <ChuanmingXie@outlook.com>
2022-07-22 06:08:38 +0800

    01_前端原生-原生语言-时尚购物商城项目-第10章HTML5进阶-绘制线条|区域

--- 01_Html_Web/ShopFashion/knowledge/Chapter10/js/10-06drawText.js
+++ 01_Html_Web/ShopFashion/knowledge/Chapter10/js/10-06drawText.js
@@ -1,29 +1,33 @@
 var canvas = document.getElementById('canvasstrokeRect');
 var context = canvas.getContext('2d');
-var text = '生命诚可贵qulitity';
-context.fillText(text, 10, 30);
-/* 填充样式 */
-context.fillStyle = 'red';
-context.font = 'italic bold 20px 隶书';
-context.fillText(text, 100, 30);
-/* 设置边框样式及文字背景 */
-context.font = '60px 隶书';
-context.fillStyle = '#20A162';
-context.fillText(text, 10, 90);
-context.strokeStyle = 'gold';
-context.strokeText(text, 10, 90);
 
-/* 使用图片作为文字边框 */
+var text = '修己安人KQZ';
+context.fillText(text, 10, 30);
+
+/* 设置填充样式font */
+context.fillStyle = 'red';
+context.font = 'italic bold 20px 楷体';
+context.fillText(text, 100, 30);
+
+/* 设置边框stroke样式及文字填充font样式 */
+context.font = '80px 楷体';
+context.fillStyle = '#20A162';
+context.fillText(text, 10, 100);
+context.lineWidth=3;
+context.strokeStyle = '#856D72';
+context.strokeText(text, 10, 100);
+
+/* 使用图片作为文字边框pattern */
 var img = new Image();
 img.src = 'img/demo/text_bg.jpg';
 img.onload = function () {
     var pattern=context.createPattern(img,'repeat');
     context.strokeStyle=pattern;
-    context.lienWidth=3;
-    context.font='60px 宋体';
+    context.lineWidth=4;
+    context.font='100px 宋体';
     context.shadowOffsetX=5;
     context.shadowOffsetY=5;
     context.shadowBlur=5;
     context.shadowColor='grey';
-    context.strokeText(text,10,190);
+    context.strokeText(text,10,200);
 }
