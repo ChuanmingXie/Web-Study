fd8b4b067fa2f8b211bc535711294d17914a4daa
ChuanmingXie <ChuanmingXie@outlook.com>
2022-07-28 19:47:14 +0800

    01_前端原生-原生语言-时尚购物商城项目-第10章HTML5进阶-多线程嵌套示例

--- 01_Html_Web/ShopFashion/knowledge/Chapter10/js/10-22luckyNumCreator.js
+++ 01_Html_Web/ShopFashion/knowledge/Chapter10/js/10-22luckyNumCreator.js
@@ -1,12 +1,12 @@
-onmessage=function(e){
-    var intArray=new Array(100);
+onmessage = function (e) {
+    var intArray = new Array(100);
     for (let i = 0; i < intArray.length; i++) {
-         intArray[i]=parseInt(Math.random()*100);
+        intArray[i] = parseInt(Math.random() * 100);
     }
     console.log(JSON.stringify(intArray));
-    var worker=new Worker('10-22luckyNumLucky.js');
+    var worker = new Worker('10-22luckyNumLucky.js');
     worker.postMessage(JSON.stringify(intArray));
-    worker.onmessage=function(e){
-        this.postMessage("");
+    worker.onmessage = function (e) {
+        postMessage(e.data);
     }
 }
--- 01_Html_Web/ShopFashion/knowledge/Chapter10/js/10-22luckyNumLucky.js
+++ 01_Html_Web/ShopFashion/knowledge/Chapter10/js/10-22luckyNumLucky.js
@@ -1,13 +1,21 @@
 onmessage = function (e) {
     var intArray = JSON.parse(e.data);
-    console.long(e.data);
-    var luckyNum = "";
+    console.log(e.data);
+    var luckyType = ['一等奖', '二等奖', '三等奖', '四等奖'];
+    var luckyNum = new Array();
+    var luckyNums = '';
+    var k = 0;
     for (let i = 0; i < intArray.length; i++) {
         if (intArray[i] % 2 == 0 && intArray[i] % 3 == 0 && intArray[i] != 0) {
-            if (luckyNum != "") {
-                luckyNum += ";"
-            }
+            luckyNum[k++] = intArray[i];
         }
-        luckyNum += intArray[i];
     }
+    k = 0;
+    for (let i = 0; i < luckyNum.length; i++) {
+        if (i % 6 == 0 && k <= 4) {
+            luckyNums += luckyType[k++] + ',';
+        }
+        luckyNums += luckyNum[i] + ',';
+    }
+    this.postMessage(luckyNums);
 }
